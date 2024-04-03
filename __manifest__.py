d2523c2b6094c15634a46cb15df3320f2030db84
AlexDev <galexramirez@yahoo.com>
2024-04-03 20:01:31 +0000

    inicio primer commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Taller',
+    'version': '1.0',
+    'author': 'Alexander Ramirez',
+    'category': 'Sales',
+    'description': 'Taller Mecanico',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/taller_registro_views.xml',
+        'views/taller_menus.xml'
+    ],
+    'license': 'LGPL-3'
+}
